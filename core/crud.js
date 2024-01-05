56838c015d7bc9de64d1b2c532b7085a5d5b252e
edmarpires9 <edmarpires18@gmail.com>
2024-01-05 19:24:51 -0300

    adicionado função que escreve na armazenamento

--- core/crud.js
+++ core/crud.js
@@ -1,4 +1,14 @@
+//Biblioteca para escrever no armazenamento persistido.
+const fs = require("fs");
+//Caminho do banco de dados
+const DB_FILE_PATH = "core/db";
+
 //Cria um content na memória
 function create(content) {
+    //Escreve no armazenamento persistido.
+    fs.writeFileSync(DB_FILE_PATH, content);
    return content; 
 }
+
+//Chama a função create
+create("Primeiro create")
