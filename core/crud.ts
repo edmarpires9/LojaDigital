af004db759f10f3c731a56489279ef9f2fbba9e1
edmarpires9 <edmarpires18@gmail.com>
2024-01-08 12:01:48 -0300

    atualizado .gitignore

--- core/crud.ts
+++ core/crud.ts
@@ -5,18 +5,22 @@ const DB_FILE_PATH = "core/db";
 
 //Cria um content na memória
 function create(content:string) {
+    
     const todo = {
         content: content,
     }
+
+    const todos = [
+        todo,
+    ]
+
     //Escreve no armazenamento persistido.
     fs.writeFileSync(DB_FILE_PATH,JSON.stringify(todo));
    return content; 
 }
 
+//Faz a leitura de um arquivo de texto no armazenamento
 function read() {
     const db = readFileSync(DB_FILE_PATH, "utf-8");
     return db;
 }
-
-//Chama a função create
-console.log(read());
